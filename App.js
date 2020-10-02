edfd0a53bf9968f6772f66a8b82465dded87a926
deepdiver <time0909@g.skku.edu>
2020-10-02 12:39:51 +0900

    add "hello react!!" to sample react app template

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// function App() {
+class App extends Component {
+  render() {
+  return (
+    <div className="App">
+      Hello react!!
+    </div>
+  );
+}
+}
+
+export default App;
