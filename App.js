0217102cdc16ae9756282714fed0c974a4c4c11a
deepdiver <time0909@g.skku.edu>
2020-10-04 02:10:56 +0900

    Add my first use of state

--- App.js
+++ App.js
@@ -6,17 +6,23 @@ import './App.css';
 
 
 
-
 // function App() {
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      subject:{title:"Hey, I'm scuba!", sub:"I'm the head of Deep Wide Studio."},
+      content:{title:"Welcome", desc:"Hi, I'm Scuba Lee. I love to record all of my history and share with somebody else."}
+    }
+  }
   render() {
   return (
     <div className="App">
-     <Subject title="Hey, I'm scuba!" sub="I'm the head of Deep Wide Studio."></Subject>
-     <Subject title="Record" sub="Recording is one of the main keywords that I have."></Subject>
-    
+     <Subject title={this.state.subject.title} 
+     sub={this.state.subject.sub}></Subject>
      <TOC></TOC>
-     <Content title="Welcome" desc="Hi, I'm Scuba Lee. I love to record all of my history and share with somebody else."></Content>
+     <Content title={this.state.content.title} 
+     desc={this.state.content.desc}></Content>
     </div>
   );
 }
