4855e2c2e9549eb1bedbd11d58b94181b139982e
deepdiver <time0909@g.skku.edu>
2020-10-04 02:52:54 +0900

    add key to App.js

--- App.js
+++ App.js
@@ -12,17 +12,25 @@ class App extends Component {
     super(props);
     this.state={
       subject:{title:"Hey, I'm scuba!", sub:"I'm the head of Deep Wide Studio."},
-      content:{title:"Welcome", desc:"Hi, I'm Scuba Lee. I love to record all of my history and share with somebody else."}
+      content:{title:"Welcome", desc:"Hi, I'm Scuba Lee. I love to record all of my history and share with somebody else."},
+      contents:[
+      {id:1, title:"Welcome", desc:"Welcome, this is Deep Wide Studio."},
+      {id:2, title:"Learning Journey", desc:"This is the history of Learning Journey."},
+      {id:3, title:"Team Project", desc:"This is the history of Team Project."}
+    ]
     }
   }
   render() {
   return (
     <div className="App">
      <Subject title={this.state.subject.title} 
-     sub={this.state.subject.sub}></Subject>
-     <TOC></TOC>
+     sub={this.state.subject.sub}>
+     </Subject>
+     <TOC data={this.state.contents}>
+     </TOC>
      <Content title={this.state.content.title} 
-     desc={this.state.content.desc}></Content>
+     desc={this.state.content.desc}>
+     </Content>
     </div>
   );
 }
