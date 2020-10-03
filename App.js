96d60334bbb9d3d0256666a96371f0e547c50037
deepdiver <time0909@g.skku.edu>
2020-10-04 00:12:19 +0900

    add components

--- App.js
+++ App.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+class Subject extends Component{
+  render(){
+    return (
+      <header>
+      <h1>Hey, I'm scuba!</h1>
+      </header>
+    );
+  }
+}
+
+class TOC extends Component{
+  render(){
+    return (
+      <nav>
+      <ul>
+          <li><a hre="1.html"></a>Welcome</li>
+          <li><a hre="2.html"></a>Learning Journey</li>
+          <li><a hre="3.html"></a>Team Project</li>
+      </ul>
+  </nav>
+    );
+  }
+}
+
+class Content extends Component{
+  render(){
+    return(
+      <article>
+      <h2>Welcome</h2> 
+      Hi, I'm Scuba Lee. I love to record all of my history and share with somebody else. 
+    </article>
+    )
+  }
+}
 
 // function App() {
 class App extends Component {
   render() {
   return (
     <div className="App">
-      Hello react!!
+     <Subject></Subject>
+     <TOC></TOC>
+     <Content></Content>
     </div>
   );
 }
